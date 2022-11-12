ec0c53e93b8b36e6cd90868a201f5ab2bf8edfc2
sandramelovic <73533914+sandramelovic@users.noreply.github.com>
2022-11-12 15:24:15 +0100

    One info

--- Ryujinx.HLE/HOS/Services/Hid/Types/Npad/ControllerType.cs
+++ Ryujinx.HLE/HOS/Services/Hid/Types/Npad/ControllerType.cs
@@ -3,7 +3,7 @@ using System;
 namespace Ryujinx.HLE.HOS.Services.Hid
 {
     [Flags]
-    public enum ControllerTypes
+    public enum ControllerType : int
     {
         None,
         ProController  = 1 << 0,
