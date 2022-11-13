a024fffdba143c6f77fa936d2e2c91ed6af175f3
sandramelovic <73533914+sandramelovic@users.noreply.github.com>
2022-11-13 01:03:55 +0100

    fix

--- Ryujinx.Memory/Tracking/RegionHandle.cs
+++ Ryujinx.Memory/Tracking/RegionHandle.cs
@@ -404,7 +404,7 @@ namespace Ryujinx.Memory.Tracking
         {
             if (_disposed)
             {
-                throw new ObjectDisposedException();
+                throw new ObjectDisposedException(GetType().FullName);
             }
 
             _disposed = true;
