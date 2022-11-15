7852ea75ac929f2dfb31599831435122f352e8df
AmilaSalihbegovic <milasalihbegovic2@gmail.com>
2022-11-15 15:14:35 +0100

    readonly

--- Ryujinx.Common/ReleaseInformations.cs
+++ Ryujinx.Common/ReleaseInformations.cs
@@ -9,7 +9,7 @@ namespace Ryujinx.Common
     {
         private const string FlatHubChannelOwner = "flathub";
 
-        public static string BuildVersion = "%%RYUJINX_BUILD_VERSION%%";
+        public static readonly string BuildVersion = "%%RYUJINX_BUILD_VERSION%%";
         public static string BuildGitHash = "%%RYUJINX_BUILD_GIT_HASH%%";
         public static string ReleaseChannelName = "%%RYUJINX_TARGET_RELEASE_CHANNEL_NAME%%";
         public static string ReleaseChannelOwner = "%%RYUJINX_TARGET_RELEASE_CHANNEL_OWNER%%";
