efcdc667888659f322d8157f1cc8c89b5215b95c
markovicnatasa <markovicka12345@gmail.com>
2022-11-15 13:17:09 +0100

    Izmena14

--- Ryujinx.Common/ReleaseInformations.cs
+++ Ryujinx.Common/ReleaseInformations.cs
@@ -11,7 +11,7 @@ namespace Ryujinx.Common
 
         public static string BuildVersion = "%%RYUJINX_BUILD_VERSION%%";
         public static string BuildGitHash = "%%RYUJINX_BUILD_GIT_HASH%%";
-        public static string ReleaseChannelName = "%%RYUJINX_TARGET_RELEASE_CHANNEL_NAME%%";
+        private const string ReleaseChannelName = "%%RYUJINX_TARGET_RELEASE_CHANNEL_NAME%%";
         public static string ReleaseChannelOwner = "%%RYUJINX_TARGET_RELEASE_CHANNEL_OWNER%%";
         public static string ReleaseChannelRepo = "%%RYUJINX_TARGET_RELEASE_CHANNEL_REPO%%";
 
