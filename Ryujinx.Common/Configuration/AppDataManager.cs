52950454a5c6c6f047c839f3f6b5ca327244c059
markovicnatasa <markovicka12345@gmail.com>
2022-11-15 18:27:14 +0100

    IzmenaVracanjaLinija

--- Ryujinx.Common/Configuration/AppDataManager.cs
+++ Ryujinx.Common/Configuration/AppDataManager.cs
@@ -80,6 +80,7 @@ namespace Ryujinx.Common.Configuration
         private static void SetupBasePaths()
         {
             Directory.CreateDirectory(BaseDirPath);
+
             var GamesDirPathCombine =(GamesDirPath = Path.Combine(BaseDirPath, GamesDir));
             Directory.CreateDirectory(GamesDirPathCombine);
 
